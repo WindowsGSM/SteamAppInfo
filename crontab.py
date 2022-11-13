b6ef47e690bbc5142cade5bca45b43dee2ad3d3f
Battlefield Duck <hkdsethomas@gmail.com>
2022-11-14 06:07:45 +0800

    Generate latest branches data

--- crontab.py
+++ crontab.py
@@ -2,6 +2,7 @@ import json
 
 from steam.client import SteamClient
 
+
 def main():
     """Generate app info json files"""
     client = SteamClient()
@@ -11,9 +12,17 @@ def main():
         app_ids = list(map(lambda app: app['appid'], json.loads(fp.read())))
         infos = client.get_product_info(app_ids)['apps']
 
+    branches = {}
+
     for app_id in app_ids:
+        branches[app_id] = infos[app_id]['depots']['branches']
+
         with open(f'AppInfo/{app_id}.json', 'w', encoding='utf-8') as fp:
             json.dump(infos[app_id], fp, indent=4, ensure_ascii=False)
+
+    with open('branches.json', 'w', encoding='utf-8') as fp:
+        json.dump(branches, fp, indent=4, ensure_ascii=False)
+
 
 if __name__ == "__main__":
     main()
